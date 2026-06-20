f647c62a013dd20ff63e8dfcb613afbb544cac88
Sepisoton1 <qhawe.kalaluka@gmail.com>
2026-06-20 14:49:01 +0000

    Fix relative import in plugin_command.py

--- files/umbrella-core/models/plugin_command.py
+++ files/umbrella-core/models/plugin_command.py
@@ -1,7 +1,7 @@
 
 from sqlalchemy import Column, Integer, String, DateTime, Boolean
 from sqlalchemy.sql import func
-from ..database import Base
+from database import Base
 
 class PluginCommand(Base):
     __tablename__ = "plugin_commands"
